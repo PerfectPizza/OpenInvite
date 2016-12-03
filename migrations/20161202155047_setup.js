28344c407dd257e07ff72d596d2097225c34632a
MKS-students <mks.students@hackreactor.com>
2016-12-02 19:33:52 -0600

    Fully working database schema now active

--- migrations/20161202155047_setup.js
+++ migrations/20161202155047_setup.js
@@ -2,34 +2,42 @@
 exports.up = (knex, Promise) => Promise.all([
   knex.schema.createTableIfNotExists('users', table => {
     //table.increments('id').primary(); //replace increments with uuid
-    table.uuid('id').primary();
+    table.string('id');
     table.string('name');
     table.string('photo'); //image?
     table.string('email');
   }),
   knex.schema.createTableIfNotExists('events', table => {
     //table.increments('id').primary();  //replace increments with uuid
-    table.uuid('id').primary();
+    //table.uuid('id'); //.primary();
+    table.uuid('id');
     table.string('address');
     table.string('latitude');
     table.string('longitude'); //string?
     table.string('location_name');
     table.integer('start_time');
     table.integer('end_time');
-    table.foreign('creator_id').references('id').inTable('users');  //foreign key with user id
+    table.string('creator_id'); //.references('id').inTable('users');  //foreign key with user id
     table.string('description');
     table.string('title');
   }),
-  knex.schema.createTableIfNotExists('events_by_user', table => {
+  knex.schema.createTableIfNotExists('events_users', table => {
     //table.increments('id').primary(); //replace increments with uuid
-    table.uuid('id').primary();
-    table.foreign('user_id').references('id').inTable('users'); //foreign key with users id
-    table.foreign('event_id').references('id').inTable('events'); //foreign key with events id
+    //table.uuid('id'); //.primary();
+    //table.increments();
+    // table.integer('user_id').unsigned();
+    // table.foreign('user_id').references('id').inTable('users'); //integer key with users id
+    table.uuid('id');
+    table.string('user_id');
+    table.integer('event_id');
+
+    //table.integer('events_id').references('id').inTable('events'); //integer key with events id
   })
 ]);
 
 exports.down = (knex, Promise) => Promise.all([
-  knex.schema.dropTableIfExists('users'),   //might need to deal with dropping foreign keys first
-  knex.schema.dropTableIfExists('events'),
-  knex.schema.dropTableIfExists('events_by_user'),
+  knex.schema.dropTableIfExists('events_users'),
+  knex.schema.dropTableIfExists('events'),   //might need to deal with dropping integer keys first
+  knex.schema.dropTableIfExists('users'),
+
 ]);
