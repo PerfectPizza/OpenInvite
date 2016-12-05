0af5c9fce07f4f10c93e753643f8466efbad19d7
MKS-students <mks.students@hackreactor.com>
2016-12-05 13:17:14 -0600

    added functionality to the EventList component and the map img now snaps to the width of the column

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,4 +1,11 @@
 
+var dummyData = [
+  {id:1, host: 'me', description:"cat party", start_time:"8:00PM", end_time:"900PM", address:"123 fakes street", locationName:"jakes house"},
+  {id:2 ,host: 'me2', description:"cat party2", start_time:"6:00PM", end_time:"8:30PM", address:"123344 Jim Doorrr", locationName: "Hat Parlor"},
+  {id:3,host: 'me3', description:"cat party3", start_time:"4:00PM", end_time:"10:00PM", address:"83834 Barn Ave.", locationName: "The park"}
+]
+
+
 class App extends React.Component {
   constructor(){
     super()
@@ -7,29 +14,27 @@ class App extends React.Component {
 
   render(){
     return (
-    <div>
-      <p> there is supposed to be a button below this</p>
+      <div>
 
-    {/* this a boostrap layout for the page */}
-      <ReactBootstrap.Grid fluid>
-        <ReactBootstrap.Row className="show-grid">
-          <ReactBootstrap.Button bsStyle="primary"> Primary </ReactBootstrap.Button>
-        </ReactBootstrap.Row>
-        {/* A row for the map and events*/}
-        <ReactBootstrap.Row className="show-grid">
-          {/* The column where the google map is located*/}
-          <ReactBootstrap.Col md={8}>
-            Map Column
-            <Map key="MAP"/>
-          </ReactBootstrap.Col>
-          {/* The column where the events are located*/}
-          <ReactBootstrap.Col md={4}>
-            Event Column
-            <EventList key="EventList"/>
-          </ReactBootstrap.Col>
-        </ReactBootstrap.Row>
-      </ReactBootstrap.Grid>
-    </div>
+      {/* this a boostrap layout for the page */}
+        <ReactBootstrap.Grid fluid>
+
+          {/* A row for the map and events*/}
+          <ReactBootstrap.Row className="show-grid">
+            {/* The column where the google map is located*/}
+            <ReactBootstrap.Col md={8}>
+              Map Column
+              <Map key="MAP"/>
+            </ReactBootstrap.Col>
+            {/* The column where the events are located*/}
+            <ReactBootstrap.Col md={4}>
+              Event Column
+              <EventList className="eventlist" key="Events" events={dummyData}/>
+            </ReactBootstrap.Col>
+          </ReactBootstrap.Row>
+
+        </ReactBootstrap.Grid>
+      </div>
     )
   }
 
@@ -44,22 +49,52 @@ class Map extends React.Component {
   render(){
     return (
       <div>
-        <span><img src={"http://bit.ly/2gHxhQI"} alt="alttext"/>Hello</span>
+        <span>
+          <img className="mapPicture" src={"http://www.drodd.com/images15/maps10.jpg"} alt="alttext"/>
+        </span>
       </div>
     )
   }
 }
 
-class EventList extends React.Component {
-  constructor () {
-    super()
-    this.state = {}
+
+function EventList (props) {
+
+    return (
+      <div>
+        {
+          props.events.map(function(event){
+            return (
+              <Event event={event}/>
+            )
+          })
+        }
+      </div>
+    )
+}
+
+class Event extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      host: this.props.event.host,
+      description: this.props.event.description,
+      startTime: this.props.event.start_time,
+      endTime: this.props.event.end_time,
+      address: this.props.event.address,
+      locationName: this.props.event.locationName
+    }
   }
 
   render(){
+    var context = this
     return (
-      <div>
-        <span><img src={"http://calvarychapelvenice.org/wp-content/uploads/2015/09/EVENTS11.jpg"} alt="alttext"/>Hello</span>
+      <div className="event">
+        <p>Location: {context.state.locationName}</p>
+        <p>Host: {context.state.host}</p>
+        <p>Start Time: {context.state.startTime}  End Time: {context.state.endTime}</p>
+        <p>Address: {context.state.address}</p>
+        <p>Description: {context.state.description}</p>
       </div>
     )
   }
