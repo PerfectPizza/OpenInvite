3a7de6a6f5240baac9b486c9ecf5ea8897a510f6
Ari Frankel <ari.l.frankel@gmail.com>
2017-05-08 11:17:27 -0500

    add in eslint

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "extends": "standard",
+    "plugins": [
+        "standard",
+        "promise"
+    ],
+    "rules": {
+      "no-unused-vars":"off",
+      "semi": [2, "always"],
+      "no-undef": 1,
+      "prefer-const":["error", {
+        "ignoreReadBeforeAssign": false
+      }],
+      "prefer-arrow-callback": 2,
+      "no-var": 2
+   }
+};
